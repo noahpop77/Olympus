ca683ee6239ccf0dbe308fe067f2f4dae9b4573c
noahpop77 <noahpop77@gmail.com>
2025-04-01 19:48:25 -0700

    More comprehensive sync map scanning endpoint and scafolding for fallback deletion of leftover artifacts

--- game_server_service/main.go
+++ game_server_service/main.go
@@ -60,8 +60,38 @@ func main() {
 	var activeMatches sync.Map
 	var matchDataMap sync.Map
 	var matchParticipantsMap sync.Map
-	var databaseTransactionMutex sync.Mutex
 	var waitGroupMap sync.Map
+	var matchCreationDates sync.Map
+
+	var databaseTransactionMutex sync.Mutex
+
+	// go func() {
+	// 	for {
+	// 		currentTime := time.Now().Unix()
+
+	// 		matchCreationDates.Range(func(key, value interface{}) bool {
+	// 			matchCreationDate, ok := value.(int64)
+	// 			if !ok {
+	// 				return true
+	// 			}
+	// 			// 86400 is the seconds in a day
+	// 			// Every day it checks for for left over artifacts and clears them out.
+	// 			// Should be faster for performances sake, lets say an hour or 2
+	// 			// Take average game time and multiply it by 2 and use that
+	// 			// if currentTime-matchCreationDate > 86400 {
+	// 			if currentTime-matchCreationDate > 0 {
+
+	// 				matchCreationDates.Delete(key)
+	// 				activeMatches.Delete(key)
+
+	// 			}
+	// 			return true
+	// 		})
+
+	// 		time.Sleep(15 * time.Second)
+	// 		// time.Sleep(5 * time.Minute)
+	// 	}
+	// }()
 
 	// Endpoint used to expose prometheus metrics
 	http.Handle("/metrics", promhttp.Handler())
@@ -71,14 +101,9 @@ func main() {
 	// Matchmaking Service triggers this to spawn a match containing a valid matchID and 10 players that
 	// are white listed to connect to that match
 	http.HandleFunc("/spawnMatch", instrumentedHandler("/addMatch", func(w http.ResponseWriter, r *http.Request) {
-
-		unpackedRequest, err := UnpackCreationRequest(w, r)
-		if err != nil {
-			http.Error(w, "Could not unpack the payload", http.StatusBadRequest)
-			return
-		}
+		unpackedRequest, _ := UnpackCreationRequest(w, r)
 		activeMatches.Store(unpackedRequest.MatchID, unpackedRequest)
-
+		matchCreationDates.Store(unpackedRequest.MatchID, time.Now().Unix())
 	}))
 
 	// Each individual player connects to this endpoint while passing in their PUUID and target matchId
@@ -91,16 +116,44 @@ func main() {
 
 	// Test function for inspecting sync maps
 	http.HandleFunc("/activeMatches", instrumentedHandler("/connectToMatch", func(w http.ResponseWriter, r *http.Request) {
+		// var outString string
+		// matchDataMap.Range(func(key, value interface{}) bool {
+		// 	outString += fmt.Sprintf("%s, %s\n", key, value)
+		// 	return true
+		// })
+
+		// waitGroupMap.Range(func(key, value interface{}) bool {
+		// 	outString += fmt.Sprintf("%s, %s\n", key, value)
+		// 	return true
+		// })
+
 		var outString string
-		matchDataMap.Range(func(key, value interface{}) bool {
-			outString += fmt.Sprintf("%s, %s\n", key, value)
+		matchCreationDates.Range(func(key, value any) bool {
+			outString += fmt.Sprintf("\n------------\nmatchCreationDates: %s, %d", key, value)
 			return true
 		})
 
-		waitGroupMap.Range(func(key, value interface{}) bool {
-			outString += fmt.Sprintf("%s, %s\n", key, value)
+		activeMatches.Range(func(key, value any) bool {
+			outString += fmt.Sprintf("activeMatches: %s, %s\n", key, value)
 			return true
 		})
+
+		matchDataMap.Range(func(key, value any) bool {
+			outString += fmt.Sprintf("matchDataMap: %s, %s\n", key, value)
+			return true
+		})
+
+		matchParticipantsMap.Range(func(key, value any) bool {
+			outString += fmt.Sprintf("matchParticipantsMap: %s, %s\n", key, value)
+			return true
+		})
+
+		waitGroupMap.Range(func(key, value any) bool {
+			outString += fmt.Sprintf("waitGroupMap: %s, %s\n", key, value)
+			return true
+		})
+
+		log.Printf("%s", outString)
 
 		w.Write([]byte(outString))
 	}))
